701e79c2805aa8179620c5f27c7400dbe946ef22
OllieMomento <ollie.chvila@gmail.com>
2025-10-20 13:48:04 +0200

    init sveltekit + tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// SPA mód: nevyrábí se HTML pro každou stránku, vše řeší index.html
+		adapter: adapter({
+			fallback: 'index.html', // důležité pro SPA
+			strict: false           // neřeší "dynamické" routy při buildu
+		}),
+		prerender: { entries: [] } // řekni SvelteKitu, ať nic necrawlí/prerenderuje
+		// Pokud web poběží v podadresáři:
+		// paths: { base: '/NAZEV' }
 	}
 };
 
